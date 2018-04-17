bd9462117e326456a7ad4fa9e3ab39310be72a80
xwp <xwp_fullstack@163.com>
2018-04-17 16:06:42 +0800

    Initial commit

---
+++ apps/user/migrations/0001_initial.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.utils.timezone
+import django.contrib.auth.models
+import django.core.validators
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('auth', '0006_require_contenttypes_0002'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='User',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
+                ('password', models.CharField(max_length=128, verbose_name='password')),
+                ('last_login', models.DateTimeField(blank=True, verbose_name='last login', null=True)),
+                ('is_superuser', models.BooleanField(default=False, verbose_name='superuser status', help_text='Designates that this user has all permissions without explicitly assigning them.')),
+                ('username', models.CharField(validators=[django.core.validators.RegexValidator('^[\\w.@+-]+$', 'Enter a valid username. This value may contain only letters, numbers and @/./+/-/_ characters.', 'invalid')], unique=True, verbose_name='username', max_length=30, error_messages={'unique': 'A user with that username already exists.'}, help_text='Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only.')),
+                ('first_name', models.CharField(max_length=30, blank=True, verbose_name='first name')),
+                ('last_name', models.CharField(max_length=30, blank=True, verbose_name='last name')),
+                ('email', models.EmailField(max_length=254, blank=True, verbose_name='email address')),
+                ('is_staff', models.BooleanField(default=False, verbose_name='staff status', help_text='Designates whether the user can log into this admin site.')),
+                ('is_active', models.BooleanField(default=True, verbose_name='active', help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.')),
+                ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
+                ('create_time', models.DateTimeField(auto_now_add=True, verbose_name='创建时间')),
+                ('update_time', models.DateTimeField(auto_now=True, verbose_name='更新时间')),
+                ('is_delete', models.BooleanField(default=False, verbose_name='删除标记')),
+                ('groups', models.ManyToManyField(verbose_name='groups', related_query_name='user', blank=True, to='auth.Group', related_name='user_set', help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.')),
+                ('user_permissions', models.ManyToManyField(verbose_name='user permissions', related_query_name='user', blank=True, to='auth.Permission', related_name='user_set', help_text='Specific permissions for this user.')),
+            ],
+            options={
+                'verbose_name_plural': '用户',
+                'db_table': 'df_user',
+                'verbose_name': '用户',
+            },
+            managers=[
+                ('objects', django.contrib.auth.models.UserManager()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Address',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
+                ('create_time', models.DateTimeField(auto_now_add=True, verbose_name='创建时间')),
+                ('update_time', models.DateTimeField(auto_now=True, verbose_name='更新时间')),
+                ('is_delete', models.BooleanField(default=False, verbose_name='删除标记')),
+                ('receiver', models.CharField(max_length=20, verbose_name='收件人')),
+                ('addr', models.CharField(max_length=256, verbose_name='收件地址')),
+                ('zip_code', models.CharField(max_length=6, verbose_name='邮政编码', null=True)),
+                ('phone', models.CharField(max_length=11, verbose_name='联系电话')),
+                ('is_default', models.BooleanField(default=False, verbose_name='是否默认')),
+                ('user', models.ForeignKey(verbose_name='所属账户', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'verbose_name_plural': '地址',
+                'db_table': 'df_address',
+                'verbose_name': '地址',
+            },
+        ),
+    ]
